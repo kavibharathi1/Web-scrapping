1fc3178b77e7c1c62b0025d4d987c325ecb98bc3
kavibharathi1 <108264391+kavibharathi1@users.noreply.github.com>
2022-07-01 10:10:29 +0530

    Add files via upload

---
+++ date.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+from htmldate import find_date
+
+
+response = requests.get("https://myfwc.com/hunting/season-dates/").content
+find_date=find_date("https://myfwc.com/hunting/season-dates/")
+    #print(response)
+soup = BeautifulSoup(response, 'lxml')
+htmldate = soup.div("div",{"class" : "col-md"})
+print(htmldate)
+    #lines = div.find_all('t')
+    #print(lines)
