65fc670792eb9268a6c0475a42379eb30b32e487
kavibharathi1 <108264391+kavibharathi1@users.noreply.github.com>
2022-07-06 15:58:13 +0530

    Update scraping link.py

--- scraping link.py
+++ scraping link.py
@@ -1,15 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-def extracting(url):
-    response =requests.get(url=url).content
-    soup =BeautifulSoup(response, 'lxml')
-    image=soup.find_all("img" , {"class": "mntl-lightbox__img"})
-    print(image)
-    for img in image:
-        print(img["scr"])
-extracting(url ="https://www.lifewire.com/download-free-books-3482754")
-
+def extractlink(url):
+    response = requests.get(url=url).content
+    soup = BeautifulSoup(response,'lxml')
+    for link in soup.find_all('a'):
+     print(link.get('href'))
+extractlink(url ="https://mdc.mo.gov/hunting-trapping/regulations")
 
 
 
