dc9d5d255de5ebd4a9a10df0bbbda734747a97e9
kavibharathi1 <108264391+kavibharathi1@users.noreply.github.com>
2022-07-20 13:25:27 +0530

    Update text.py

--- text.py
+++ text.py
@@ -5,8 +5,9 @@ def ExtractData(url):
     response = requests.get(url=url).content
     #print(response)
     soup = BeautifulSoup(response, 'lxml')
-    div = soup.div("div",{"class" : "loc article-content"})
-    print(div)
+    div = soup.div("a",{"class" : "mntl-sc-block-heading__link"})
+    for each in div:
+       print(each.text)
     #lines = div.find_all('t')
     #print(lines)
 
