2e8d368a9b3abcbf9123a85db20a98f3f262a29e
kavibharathi1 <108264391+kavibharathi1@users.noreply.github.com>
2022-08-09 18:40:37 +0530

    Update species_season name_season date_.py

--- species_season name_season date_.py
+++ species_season name_season date_.py
@@ -1,26 +1,28 @@
 import pandas as pd
-import numpy as np
 import requests
 from bs4 import BeautifulSoup
 
 season_date =[]
 species_name =[]
-pages = np.arange(1,2,10)
-for page in pages:
-     page =requests.get("https://mdc.mo.gov/hunting-trapping/seasons?species_topic_filter=All&page=0")
-     soup=BeautifulSoup(page.text,'html.parser')
-     data =soup.find_all('div',attrs={'role':'article'})#'id':'-content'
 
-     for store in data:
 
-         name_1= store.h2.a.find('span', class_ ='field--title').text
+url =("https://mdc.mo.gov/hunting-trapping/seasons?species_topic_filter=All&page=0")
+response=requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+data = soup.find_all('div', attrs={'role': 'article'})
+#print(data)  # 'id':'-content'
+
+
+for store in data:
+
+         name_1= store.find('span',class_='field--title').text.replace('\n','')
          species_name.append(name_1)
-         name_2 = store.find(class_='field__items').text.replace('\n', '')
+
+         name_2 = store.find(class_='field__item').text
          season_date.append(name_2)
 
 list= pd.DataFrame({"species name":species_name,"season date":season_date})
 print(list)
-
 
 
 
